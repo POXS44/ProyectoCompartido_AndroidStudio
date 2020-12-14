4a63cffa94134da5e6d7a02c05e61880c0800446
unknown <vzeledon7@gmail.com>
2020-12-13 20:39:11 -0600

    Se intento

--- app/src/main/java/com/poxs44/miapp20/AuthActivity.kt
+++ app/src/main/java/com/poxs44/miapp20/AuthActivity.kt
@@ -1,11 +1,25 @@
 package com.poxs44.miapp20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import java.net.PasswordAuthentication
+import kotlin.properties.Delegates
 
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var signUpButton : Button
+    private lateinit var logInButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -16,6 +30,8 @@ class AuthActivity : AppCompatActivity() {
     bundle.putString("message","Integración de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
+
+
         //Setup
         setup ()
     }
@@ -23,8 +39,59 @@ class AuthActivity : AppCompatActivity() {
     private fun setup (){
         title = "Autenticación"
 
+        signUpButton = findViewById(R.id.signUpButton)
+        logInButton = findViewById(R.id.logInButton)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
 
+        signUpButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
 
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?:"",ProviderType.BASIC)
+                    }
+                    else {
+                     showAlert()
+                    }
+                }
+            }
+        }
+
+        logInButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(),
+                        passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?:"",ProviderType.BASIC)
+                    }
+                    else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al Usuario")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider",provider.name)
+        }
+        startActivity(homeIntent)
     }
 
 }
--- app/src/main/java/com/poxs44/miapp20/HomeActivity.kt
+++ app/src/main/java/com/poxs44/miapp20/HomeActivity.kt
@@ -2,10 +2,46 @@ package com.poxs44.miapp20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType{
+        BASIC
+    }
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var closeButton: Button
+    private lateinit var emailTextView: TextView
+    private lateinit var providerTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Setup
+        val bundle : Bundle? = intent.extras
+        val email : String? = bundle?.getString("email")
+        val provider : String? = bundle?.getString("provider")
+        setup(email?:"",provider?:"")
+
+    }
+
+    private fun setup (email: String, provider: String){
+
+        title = "Inicio"
+
+        emailTextView = findViewById(R.id.emailEditText)
+        providerTextView = findViewById(R.id.providerTextView)
+
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        closeButton.setOnClickListener(){
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }

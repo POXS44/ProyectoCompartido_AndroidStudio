ff926d578655f294d28b952092ea5fea2b386f59
unknown <vzeledon7@gmail.com>
2020-12-13 19:42:47 -0600

    Codigo

--- app/src/main/java/com/poxs44/miapp20/AuthActivity.kt
+++ app/src/main/java/com/poxs44/miapp20/AuthActivity.kt
@@ -2,10 +2,30 @@ package com.poxs44.miapp20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+    // Analytics Event
+    val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    val bundle = Bundle()
+    bundle.putString("message","Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        //Setup
+        setup ()
     }
+
+    private fun setup (){
+        title = "Autenticación"
+
+
+
+    }
+
 }
+
